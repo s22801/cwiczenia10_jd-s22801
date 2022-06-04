0b54ce40238f7c5b74ddb4051353db7e66da4ec9
Sylwia Staroń <s22801@pjwstk.edu.pl>
2022-06-04 22:09:10 +0200

    Microsoft tutorial MVC Web Apps (up to Examine Details and Delete)

---
+++ APBD10/APBD10/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APBD10.Models;
+
+namespace APBD10.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APBD10.Models.Movie> Movie { get; set; }
+    }
+}
